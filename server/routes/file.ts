5e217d81da89ca66f1dc8c47e0562106c64c8b9b
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-10-08 19:14:19 +0530

    cloudinary added

--- server/routes/file.ts
+++ server/routes/file.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import multer from "multer";
+import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
+import File from "../models/File";
 const router = express.Router();
 
 let storage = multer.diskStorage({});
@@ -8,10 +10,32 @@ let upload = multer({
   storage,
 });
 
-router.post("/upload", upload.single("myFile"), (req, res) => {
+router.post("/upload", upload.single("myFile"), async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ message: "Need the file" });
-    console.log(req.file);
+    let uploadedFile: UploadApiResponse;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "shareMe",
+        resource_type: "auto",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: "cloudinary error" });
+    }
+    const { originalname } = req.file;
+    const { secure_url, bytes, format } = uploadedFile;
+    // console.log(uploadedFile);
+    const file = await File.create({
+      filename: originalname,
+      sizeInBytes: bytes,
+      secure_url,
+      format,
+    });
+    res.status(200).json({
+        id:file._id,
+        Link:`${process.env.API_BASE_ENDPOINT_CLIENT}download/${file._id}`
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Sever error" });
--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,17 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
 import fileRouter from "./routes/file";
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
 const app = express();
+
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_API_CLOUD,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
 connectDB();
 app.use(cors());
 app.use(express.json());
